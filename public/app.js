15c1c80d3e475417b155354e20cf8137412ab9ce
syon <syon.xiii@gmail.com>
2015-05-24 16:38:42 +0900

    Material Angular

--- public/app.js
+++ public/app.js
@@ -1,13 +1,20 @@
-var myApp = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngMaterial']);
 
-myApp.controller('appCtrl', function ($scope, $http) {
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http){
+
+  $scope.ing = false;
+
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
 
   $scope.click = function() {
-    $scope.ajaxstate = "processing...";
+    $scope.ing = true;
     $http.post('/').success(function(data) {
+      console.log(data);
       $scope.articles = data;
-      $scope.ajaxstate = "";
+      $scope.ing = false;
     });
   };
 
-});
+}]);
