62820f3f2ce8ff3f5a59a70a8ecfe685a4709c6b
syon <syon.xiii@gmail.com>
2015-05-27 00:13:54 +0900

    Ajax error handling

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ var app = angular.module('myApp', ['ngMaterial']);
 app.controller('AppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http){
 
   $scope.ing = false;
+  $scope.err = null;
 
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
@@ -10,11 +11,21 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $md
 
   $scope.click = function() {
     $scope.ing = true;
-    $http.post('/').success(function(data) {
-      console.log(data);
-      $scope.articles = data;
-      $scope.ing = false;
-    });
+    $http.post('/')
+      .success(function(data) {
+        $scope.ing = false;
+        console.log(data);
+        $scope.articles = data;
+      })
+      .error(function(data, status, headers, config) {
+        $scope.ing = false;
+        $scope.err = "Error! -- data:" + data + "  status:" + status
+        console.error("data: ", data);
+        console.error("status: ", status);
+        console.error("headers: ", headers);
+        console.error("config: ", config);
+      })
+    ;
   };
 
 }]);
